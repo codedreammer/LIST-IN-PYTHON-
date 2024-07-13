4fa0b06f7e3aeb7aea737839fb37d38fde08c9c2
codedreammer <nkanal38@gmail.com>
2024-07-13 18:58:14 +0530

    add example file

---
+++ example of list .py
@@ -0,0 +1,6 @@
+animals=["cat","dog","cow","lion","horse","donkey","monkey","kobra","akshay","alok"]
+print(animals[::2])
+print(animals[-8:-1:2])
+
+list = ["apple","mango","orange","kiwi"]
+print(list[-3:-1])
