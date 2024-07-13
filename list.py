f256815fc907a3ca1b67d3db059bf5d45e358d16
codedreammer <nkanal38@gmail.com>
2024-07-13 18:14:28 +0530

    add lists

--- list.py
+++ list.py
@@ -1,9 +1,31 @@
-marks = [3,5, 6, "Herry", True]
+marks = [3,5, 6, "Akshay", True,'ok']
+#print(marks)
+#print(type(marks))
+#print(marks[0])
+#print(marks[1])
+#print(marks[2])
+#print(marks[3])
+#print(marks[4])
+#print(marks[5])
+
+#print(marks[-3])
+#print(marks[len(marks)-3])
+
+#print(marks[5-3])
+#print(marks[2])
+
+if "6" in marks:
+    print("yes")
+else:
+    print("no")
+
+if "arry" in "akshay":
+    print("yes")
+else:
+    print("no")
+
+
 print(marks)
-print(type(marks))
-print(marks[0])
-print(marks[0])
-print(marks[0])
-print(marks[0])
-print(marks[0])
-print(marks[0])
+print(marks[1:3])
+print(marks[1:])
+
