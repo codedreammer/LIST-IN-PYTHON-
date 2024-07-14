32a2e5eb4ac6d469e973b3f34919d47dd1f67d07
codedreammer <nkanal38@gmail.com>
2024-07-14 17:09:15 +0530

    add the file

---
+++ list method.py
@@ -0,0 +1,13 @@
+l = [1,2,3,4,34,23]
+print(l)
+#l.append(7)
+#l.sort()
+#l.sort(reverse=True)
+#l.reverse()
+#m = l.copy()
+#m[0]=0
+m = [900 , 1000, 1100]
+#l.extend(m)
+k = l+m
+print(k)
+print(l)
