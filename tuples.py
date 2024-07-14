bceb2a7cadfd72fd042b22fcc8e407c60346f96c
codedreammer <nkanal38@gmail.com>
2024-07-14 20:36:58 +0530

    add the file of tuples

---
+++ tuples.py
@@ -0,0 +1,13 @@
+tup = (1, 5, 76, 343, 500)
+#tup[0]= 60
+print(type(tup),tup)
+
+tup1=(1,2,3,4,5)
+tup2=("red","black","orange")
+print(tup1)
+print(tup2)
+
+if 343 in tup:
+    print("yes 343 is present in the tup")
+    tup2= tup[:2]
+    print(tup2)
