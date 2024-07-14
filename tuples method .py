e507c81661302b80ecd8043b4adc4cd5b895ddf4
codedreammer <nkanal38@gmail.com>
2024-07-14 20:59:29 +0530

    add the example

--- tuples method .py
+++ tuples method .py
@@ -6,3 +6,11 @@ temp.pop(3)           #remove item
 temp[2] = "finland"    #change item
 countries = tuple(temp)
 print(countries)
+
+name = ("akshay","alok","shorya","lucky")
+change =list(name)
+change.append("ayush")
+change.pop(4)
+change[2] ="shubham"
+name = tuple(change)
+print(name)
